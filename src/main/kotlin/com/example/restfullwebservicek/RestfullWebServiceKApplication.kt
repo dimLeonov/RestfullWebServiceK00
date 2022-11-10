aa508e8cef8ca82294f10b9590e3b3ea47170fe1
dmitryleonov <dim.leonov97@gmail.com>
2022-11-10 02:30:05 +0200

    https://spring.io/guides/gs/rest-service/

---
+++ src/main/kotlin/com/example/restfullwebservicek/RestfullWebServiceKApplication.kt
@@ -0,0 +1,11 @@
+package com.example.restfullwebservicek
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestfullWebServiceKApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestfullWebServiceKApplication>(*args)
+}
---
+++ src/main/kotlin/com/example/restfullwebservicek/controller/GreetingController.kt
@@ -0,0 +1,4 @@
+package com.example.restfullwebservicek.controller
+
+class GreetingController {
+}
